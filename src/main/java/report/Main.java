78e6b6f65f80c8ced4f8738a9a70e1dcb85177ab
Alexey <slabkin.alexey@gmail.com>
2020-10-15 23:08:03 +0300

    Big refactor

--- src/main/java/report/Main.java
+++ src/main/java/report/Main.java
@@ -13,8 +13,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         WritingAReports.getReport(Choise.getBoC(), Choise.getPlatform());
-
     }
 }
