808afe3c129c6aed4aa579e980f48706c509ddc5
Alexey <slabkin.alexey@gmail.com>
2020-10-22 00:49:22 +0300

    Add password for start tool

--- src/main/java/report/mainClass/Main.java
+++ src/main/java/report/mainClass/Main.java
@@ -2,10 +2,24 @@ package main.java.report.mainClass;
 
 import main.java.report.GetData;
 import main.java.report.Reporting;
+import main.java.report.Scan;
 
 public class Main {
 
     public static void main(String[] args) {
-        Reporting.getReport(GetData.getBoC(), GetData.getPlatform());
+        System.out.println("\nWelcome to LOGAN v1.1\n");
+        retry();
+    }
+
+    public static void retry() {
+        System.out.println("You have to enter the password: ");
+        String pass = Scan.scanText();
+        if (pass.equals("ajaxtest")) {
+            System.out.println("\n");
+            Reporting.getReport(GetData.getBoC(), GetData.getPlatform());
+        } else {
+            System.out.println("\nSorry, wrong password! Try again.\n");
+            retry();
+        }
     }
 }
