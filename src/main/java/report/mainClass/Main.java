ea7d6e52675b31cde6b18029879fd7e94c8261d7
slabkin_alexey <slabkin.alexey@gmail.com>
2021-03-09 14:51:50 +0200

    clean up

--- src/main/java/report/mainClass/Main.java
+++ src/main/java/report/mainClass/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        System.out.print("\nWelcome to LOGAN v1.8.3" +
+        System.out.print("\nWelcome to LOGAN v1.8.4" +
                 "" +
                 "\n");
         System.out.println("When security is art!\n");
