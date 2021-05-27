bed1f7665d468866d2ce1ee7a008daec11e6f1a1
slabkin_alexey <slabkin.alexey@gmail.com>
2021-05-27 11:02:44 +0300

    clean up

--- src/main/java/report/mainClass/Main.java
+++ src/main/java/report/mainClass/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         String username = System.getProperty("user.name");
 
-        System.out.print("\nWelcome to LOGAN v2.0, "+username +"!\n\n");
+        System.out.print("\nWelcome to LOGAN v2.0.1, "+username +"!\n\n");
         retry();
     }
 
