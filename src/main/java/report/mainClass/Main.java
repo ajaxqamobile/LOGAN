1cb995308d27f0d51fd91041fba8d7bcf0b0ea5b
slabkin_alexey <slabkin.alexey@gmail.com>
2021-10-01 11:53:31 +0300

    increment version app

--- src/main/java/report/mainClass/Main.java
+++ src/main/java/report/mainClass/Main.java
@@ -3,7 +3,6 @@ package main.java.report.mainClass;
 import main.java.report.GetData;
 import main.java.report.Reporting;
 
-import javax.swing.*;
 import java.io.IOException;
 
 public class Main {
@@ -11,7 +10,7 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         String username = System.getProperty("user.name");
 
-        System.out.print("\nWelcome to LOGAN v2.0.6, "+username +"!\n\n");
+        System.out.print("\nWelcome to LOGAN v2.0.7, "+username +"!\n\n");
         retry();
     }
 
