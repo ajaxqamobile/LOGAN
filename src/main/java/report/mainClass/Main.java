8c29b605de459ab0111afc574dc0b39d20453467
ajaxqamobile <ajax.internal@gmail.com>
2022-02-15 10:47:28 +0200

    2.4.9 minor upd

--- src/main/java/report/mainClass/Main.java
+++ src/main/java/report/mainClass/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         String username = System.getProperty("user.name");
 
-        String version = "2.4.5.3";
+        String version = "2.4.9";
 
         System.out.print("\nДобро пожаловать в LOGAN [v"+version+"], "+username +"!\n");
         System.out.println(getDate());
